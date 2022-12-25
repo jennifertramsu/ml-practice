a14a39a50da0dfff1c2dfd17cf23d27248790580
jennifertramsu <jennifer.tramsu@gmail.com>
2022-12-25 14:59:27 -0500

    preprocess

--- helper.py
+++ helper.py
@@ -1,16 +1,52 @@
 import librosa
 
-def preprocess(files, sr):
-    ''' This function will take in a list of paths for audio files and output them as floating-point time series.
+def preprocess(file):
+    ''' 
+    Parameters
+    ----------
+    file : str
+        The path to the audio file to be loaded.
+
+    Returns
+    -------
+    audio :
+
+    err : int {0, 1}
+        Error code. 
+
+    '''
+    err = 0
+
+    try:
+        audio = librosa.load(file)
+    except:
+        print(file + " is corrupted!")
+        err = 1
+        audio = None
+    finally:
+        return audio, err
+
+def process_train(X_train, y_train):
+    ''' This function will process the training files.
 
     Parameters
     ----------
 
+
     Returns
     -------
-
+    
     '''
+    X_audio = []
 
-    for path in files:
-        audio = librosa.load(path, sr=sr)
-        print(audio)
+    for i in range(len(X_train)):
+        path = X_train.iloc[i]
+        audio, err = preprocess(path)
+
+        if err:
+            del y_train[i]
+            i -= 1
+        else:
+            X_audio.append(audio)
+
+    return X_audio, y_train
